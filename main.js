54c935854e8d209742bba5fd0ec525715b0d4cae
John Bauer <johnbauer126@gmail.com>
2018-07-01 16:52:31 -0400

    set up chart.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let myChart = document.querySelector('#myChart').getContext('2d')
+
 const data = fetch('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=BTC&market=CNY&apikey=demo')
     .then(function(response){
         return response.json()
@@ -5,3 +7,9 @@ const data = fetch('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_
     .then(function(myJson){
         console.log(myJson)
     })
+
+
+let bitcoinChart = new Chart(myChart,{
+    type:'line',
+    data: []
+})
